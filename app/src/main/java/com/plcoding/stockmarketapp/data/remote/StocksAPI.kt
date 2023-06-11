028023edd5cbc19c6cf3de075abde001c977e138
Renuka Challa <challarenuka20@gmail.com>
2023-06-11 19:20:02 +0530

    1. Created Entity classes, CSVparser interface and impl, mappers, stock dao, stockdatabase, repo impl, stocks api in data package 2. Created Company listings model classes, stockrepository in domain package 3. Resource in util package

---
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StocksAPI.kt
@@ -0,0 +1,19 @@
+package com.plcoding.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StocksAPI {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apiKey") apiKey:String = API_KEY
+    ) : ResponseBody
+
+    companion object{
+        const val API_KEY = "RL7JTM607IRNO1HC"
+        const val BASE_URL = "https://alphavantage.co/"
+    }
+}
